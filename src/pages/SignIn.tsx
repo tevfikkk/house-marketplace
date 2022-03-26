1f7f2ea691782a031c1054087644b6b3120079c8
[Tevfik] <tevfikhoglu@gmail.com>
2022-03-26 23:38:48 +0300

    a

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,22 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
+interface IForm {
+  email: string
+  password: string
+}
 
 const SignIn: FC = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const [formData, setFormData] = useState<IForm>({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
   return <div>Sign-In</div>
 }
 
